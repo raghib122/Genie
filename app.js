2404eea63f194e43cbdb7b44a1c2a08a9f38e412
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-10-05 17:09:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,397 @@
+function greeting(){
+  alert("I am glad you are here Princess")
+}
+greeting()
+
+function talk(){
+    var know = {
+        "Hello": "ٱلسَّلَامُ عَلَيْكُمْ",
+        "hello": "ٱلسَّلَامُ عَلَيْكُمْ",
+        "hii": "ٱلسَّلَامُ عَلَيْكُمْ",
+        "Hii": "Welcome Princess",
+        "name": "You are princess Noureen",
+        "Name": "You are princess Noureen",
+        "your name": "Sorry my Princess, But my creator doesn't give me any name",
+        "Your name": "Sorry my Princess, But my creator doesn't give me any name",
+    "Who are you" : "I'm your personal assistant Princess, ",
+    "who are you" : "I'm your personal assistant Princess, ",
+    "who am i": "You are precious Princess",
+    "noureen" : "It's you your Highness, the precious one",
+    "Noureen" : "It's you your Highness, the precious one",
+    "raghib": "He is always there for you princess",
+    "Raghib": "He is always there for you princess",
+    "fgihjg": "Sorry Princess",
+    "How are you" : "Good :)",
+    "What can i do for you" : "Just keep silence",
+    "what can you do": "I am with you Princess",
+   "Love": "My soul and your soul are forever tangled.",
+   "love":  `Like a rose, you bloom and grow,
+   Your beauty, a wonder to behold,
+   My heart, forever yours to keep,
+   As we journey through life, step by step.` ,
+
+   "love": `Your love is a symphony,
+   A song that fills my heart;
+   Together, we are meant to be. `,
+
+   "poetry":`Like a rose, you bloom and grow,
+   Your beauty a sight to behold,
+   I am honored to be by your side,
+   Forever your love to hold. `,
+
+   "1": `Your love is my home,
+   A safe haven to call my own. `,
+
+   "2" :`I don't think you will
+   Ever fully understand
+   How you've touched my life
+   And made me who I am.
+   
+   I don't think you could ever know
+   Just how truly special you are,
+   That even on the darkest nights
+   You are my brightest star.
+   
+   You've allowed me to experience
+   Something very hard to find,
+   Unconditional love that exists
+   In my body, soul, and mind.
+   
+   I don't think you could ever feel
+   All the love I have to give,
+   And I'm sure you'll never realize
+   You've been my will to live.
+   
+   You are an amazing person,
+   And without you I don't know where I'd be.
+   Having you in my life
+   Completes and fulfills every part of me.`,
+
+   "3":`If you’re feeling down, turn your frown upside down.
+   Put a smile on your face; take the world in your embrace.
+   Ask for a little help from the man up above.
+   And remember you have your best friend’s love.`,
+
+   "4":`The beauty of friendship lies in the heart
+   You have a special place from the start
+   You care and I can see in your eyes
+   All the things I have learned so wise
+   I have learnt from you my friend
+   There is nothing that I can pretend
+   Except for the reason that I love you
+   And I simply know its true!
+   My friend I love you!`,
+
+   "4": `In Noureen's effortlessness, a brilliant light,
+   Charming, kind, and insightful, a good nature's flight.
+   I'm close by, through various challenges,
+   In your viewpoints, my dear, you've forever been.
+   
+   A princess uncommon, inside and out,
+   In your presence, more splendid sparkles the day.`,
+
+   "5": `In Noureen's presence, beauty takes off,
+   Charming, kind, and savvy, a directing light.
+   Her heart, a fortune, unadulterated and true,
+   In each idea, I'm there for you.
+   
+   With mind that takes off and astuteness' might,
+   She fills our reality with a brilliant light.
+   Noureen, a princess, all around,
+   In your brilliant air, we track down our day.`,
+
+   "6":`Dosti Hoti Nahi Bhul Jane Ke Liye,Dost Milte Nahi Bikhar Jane Ke Liye,Dosti Karke Khush Rahoge Itna,Ki Waqt Milega Nai Aansu Bahane Ke Liye. .`,
+
+   "7":`Out of your love the fire of youth will rise.
+   In the chest, visions of the soul will rise.
+   If you are going to kill me, kill me, it is alright.
+   When the friend kills, a new life will rise.`,
+
+   "8":`This is love: to fly toward a secret sky,
+   to cause a hundred veils to fall each moment.
+   First, to let go of live.
+   In the end, to take a step without feet;
+   to regard this world as invisible,
+   and to disregard what appears to be the self.
+   
+   Heart, I said, what a gift it has been
+   to enter this circle of lovers,
+   to see beyond seeing itself,
+   to reach and feel within the chest.`,
+
+   "9": `A certain person came to the Friend's door
+   and knocked.
+   "Who's there?"
+   "It's me."
+   The Friend answered, "Go away. There's no place
+   for raw meat at this table."
+   
+   
+   The individual went wandering for a year.
+   Nothing but the fire of separation
+   can change hypocrisy and ego. The person returned
+   completely cooked,
+   walked up and down in front of the Friend's house,
+   gently knocked.
+   "Who is it?"
+   "You."
+   "Please come in, my self,
+   there's no place in this house for two.
+   The doubled end of the thread is not what goes through
+   the eye of the needle.
+   It's a single-pointed, fined-down, thread end,
+   not a big ego-beast with baggage."`,
+
+   "10": `What hidden sweetness there is in this emptiness of the belly!
+   Man is surely like a lute, no more and no less;
+   For if, for instance, the belly of the lute becomes full, no
+   lament high or low will arise from that full lute.
+   If your brain and belly are on fire through fasting, because of
+   the fire every moment a lament will arise from your breast.
+   Every moment you will burn a thousand veils by that fire; you
+   will mount a hundred steps with zeal and endeavor.
+   Become empty of belly, and weep entreatingly like the reed
+   pipe; become empty of belly, and tell secrets with the reed pen.
+   If your belly is full at the time of concourse, it will bring Satan
+   in place of your reason, an idol in place of the Kaaba.
+   When you keep the fast, good habits gather together before
+   you like slaves and servants and retinue.
+   Keep the fast, for that is Solomon’s ring; give not the ring to
+   the div, destroy not your kingdom.
+   Even if your kingdom has gone from your head and your army
+   has fled, your army will rise up, pennants flying above them.
+   The table arrived from heaven to the tents of the fast, by the
+   intervention of the prayers of Jesus, son of Mary.
+   In the fast, be expectant of the table of bounty, for the table of
+   bounty is better than the broth of cabbages.`,
+
+   "11": `You personify God's message.
+   You reflect the Queen's face.
+   There is nothing in the universe that you are not
+   Everything you want, look for it within yourself--
+   you are that.`,
+
+   "12": `“Out beyond ideas of wrongdoing
+   and rightdoing there is a field.
+   I’ll meet you there.
+   When the soul lies down in that grass
+   the world is too full to talk about.”`,
+
+   "13":`“The minute I heard my first love story,
+   I started looking for you, not knowing
+   how blind that was.
+   Lovers don’t finally meet somewhere.
+   They’re in each other all along.”`,
+
+   "14": `“I want to see you.
+   Know your voice. Recognize you when you
+   first come ’round the corner. Sense your scent when I come
+   into a room you’ve just left. Know the lift of your heel,
+   the glide of your foot. Become familiar with the way
+   you purse your lips
+   then let them part,
+   just the slightest bit,
+   when I lean in to your space
+   and kiss you. I want to know the joy
+   of how you whisper
+   “more”`,
+
+   "14":`“I said to the night,
+   “If you are in love with the moon,
+   it is because you never stay for long.”
+   The night turned to me and said,
+   “It is not my fault. I never see the Sun,
+   how can I know that love is endless?”`,
+
+   "15": `“If I love myself
+   I love you.
+   If I love you
+   I love myself.”`,
+
+   "16": `“Lovers find secret places
+   inside this violent world
+   where they make transactions
+   with beauty.”`,
+
+   "17": `“If you are seeking, seek us with joy
+   For we live in the kingdom of joy.
+   Do not give your heart to anything else
+   But to the love of those who are clear joy,
+   Do not stray into the neighborhood of despair.
+   For there are hopes: they are real, they exist –
+   Do not go in the direction of darkness –
+   I tell you: suns exist.”`,
+
+   "18": `“Love isn’t the work of the tender and the gentle;
+   Love is the work of wrestlers.
+   The one who becomes a servant of lovers
+   is really a fortunate sovereign.
+   Don’t ask anyone about Love; ask Love about Love.
+   Love is a cloud that scatters pearls.”`,
+
+   "a":`“This is how I would die
+   into the love I have for you:
+   As pieces of cloud
+   dissolve in sunlight.”`,
+
+   "A": `“Love so needs to love
+   that it will endure almost anything, even abuse,
+   just to flicker for a moment. But the sky’s mouth is kind,
+   its song will never hurt you, for I
+   sing those words.”`,
+
+   "b": `    How long wilt thou dwell on words and superficialities?
+
+   A burning heart is what I want; consort with burning.
+
+   Kindle in thy heart the flame of Love,
+
+   And burn up utterly thoughts and fine expressions.`,
+
+   "c":`Love’s way is humility and intoxication,
+   The torrent floods down. How can it run up?
+   You’ll be a cabuchon in the ring of lovers,
+   If you’re a red ruby’s slave, dear friend ;
+   Even as Earth is a serf of the sapphire sky
+   And your monkey body’s a slave to your spirit.
+   
+   What did Earth ever lose by this relationship ?
+   What mercy has the Self showed to weary limbs ?
+   One shouldn’t beat the snare drum of awakening
+   Beneath a cosy sofa’s, comfy counterpane.
+   
+   Hoist, like a hero, your flag in the desert.
+   Listen with your soul’s ear to the song,
+   In that hollow of the vast turquoise dome,
+   Rising from the lover’s passionate moan .
+   
+   When your tight gown-strings are loosened
+   By the tipsy inebriation of perfect love,
+   The victorious heavens shout, triumphantly !
+   And the constellations gaze down ashamed.
+   This world is in deep trouble, from top to bottom,
+   But it can be swiftly healed by the balm of love .`,
+
+   
+
+
+
+
+
+
+    "Rumi": `If anyone asks you
+    how the perfect satisfaction
+    of all our sexual wanting
+    will look, lift your face
+    and say,
+    
+    Like this.
+    
+    When someone mentions the gracefulness
+    of the nightsky, climb up on the roof
+    and dance and say,
+    
+    Like this.
+    
+    If anyone wants to know what “spirit” is,
+    or what “God’s fragrance” means,
+    lean your head toward him or her.
+    Keep your face there close.
+    
+    Like this.
+    
+    When someone quotes the old poetic image
+    about clouds gradually uncovering the moon,
+    slowly loosen knot by knot the strings
+    of your robe.
+    
+    Like this.
+    
+    If anyone wonders how Jesus raised the dead,
+    don’t try to explain the miracle.
+    Kiss me on the lips.
+    
+    Like this. Like this.
+    
+    When someone asks what it means
+    to “die for love,” point
+    here.
+    
+    If someone asks how tall I am, frown
+    and measure with your fingers the space
+    between the creases on your forehead.
+    
+    This tall.
+    
+    The soul sometimes leaves the body, the returns.
+    When someone doesn’t believe that,
+    walk back into my house.
+    
+    Like this.
+    
+    When lovers moan,
+    they’re telling our story.
+    
+    Like this.
+    
+    I am a sky where spirits live.
+    Stare into this deepening blue,
+    while the breeze says a secret.
+    
+    Like this.
+    
+    When someone asks what there is to do,
+    light the candle in his hand.
+    
+    Like this.
+    
+    How did Joseph’s scent come to Jacob?
+    
+    Huuuuu.
+    
+    How did Jacob’s sight return?
+    
+    Huuuu.
+    
+    A little wind cleans the eyes.
+    
+    Like this.
+    
+    When Shams comes back from Tabriz,
+    he’ll put just his head around the edge
+    of the door to surprise us
+    
+    Like this.`,
+
+    "good morning" : "Good morning, my radiant princess. With the sunrise, your grace and beauty shine anew. May this day be as lovely and inspiring as you are. Embrace the moments ahead with the same elegance that defines you. 🌞👑💖",
+    "Good Morning" : "Good morning, my radiant princess. With the sunrise, your grace and beauty shine anew. May this day be as lovely and inspiring as you are. Embrace the moments ahead with the same elegance that defines you. 🌞👑💖",
+
+    "Good Night" : "Good night, my dear princess. As the stars twinkle in the sky, may your dreams be as beautiful as your heart. Rest well, for when morning comes, a new day of enchantment awaits. Sleep tight. 🌙👑💫",
+    "good night" : "Good night, my dear princess. As the stars twinkle in the sky, may your dreams be as beautiful as your heart. Rest well, for when morning comes, a new day of enchantment awaits. Sleep tight. 🌙👑💫",
+
+    
+    "ok" : "Thank You So Much ",
+    "what is your favorite hobby": "I enjoy reading books and learning new things.",
+    "tell me a joke": "Sure, here's one: Why don't scientists trust atoms? Because they make up everything!",
+    "what is your favorite music genre": "I appreciate various genres of music, but I particularly enjoy listening to classical music.",
+    "can you recommend a good book": "Certainly! I highly recommend 'Sapiens: A Brief History of Humankind' by Yuval Noah Harari.",
+    "What is your favorite hobby": "I enjoy reading books and learning new things.",
+    "Tell me a joke": "Sure, here's one: Why don't scientists trust atoms? Because they make up everything!",
+    "What is your favorite music genre": "I appreciate various genres of music, but I particularly enjoy listening to classical music.",
+    "Can you recommend a good book": "Certainly! I highly recommend 'Sapiens: A Brief History of Humankind' by Yuval Noah Harari.",
+    "Bye" : "Okay! Will meet soon, Your Highness",
+    "bye" : "Okay! Will meet soon, Your Highness",
+    };
+    var user = document.getElementById('userBox').value;
+  var chatLog = document.getElementById('chatLog');
+  var response = '';
+
+  if (user in know) {
+    response = know[user];
+  } else {
+    response = "You are precious Princess❤️👸. I appreciate your input! ❤️👸";
+  }
+
+  chatLog.innerHTML = user + "<br>";
+  chatLog.innerHTML += response + "<br>";
+}
