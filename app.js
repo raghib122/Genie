1f19d4f442cc5121470e307a1b9924861998beb1
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-10-06 13:18:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function greeting() {
   var userInput = prompt("Please enter the correct password to proceed Princess:");
-
+userInput = userInput.trim();
+  
   if (userInput === "Noureen" || userInput === "Princess") {
     alert("Welcome, Princess! I am glad you are here Princess ");
     // Redirect to the actual page or perform any other actions here
