fa20744cd3f8c1eda0c9716c8f67413ce89a31a7
raghib122 <125207689+raghib122@users.noreply.github.com>
2023-10-07 00:12:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -265,6 +265,56 @@ function talk(){
    Don’t ask anyone about Love; ask Love about Love.
    Love is a cloud that scatters pearls.”`,
 
+
+      "19":`Because I could not stop for Death,
+
+He kindly stopped for me;
+
+The carriage held but just ourselves
+
+And Immortality.`,
+
+      "20":`Shall I compare thee to a summer’s day?
+
+Thou art more lovely and more temperate:
+
+Rough winds do shake the darling buds of May,
+
+And summer’s lease hath all too short a date;
+
+Sometime too hot the eye of heaven shines,
+
+And often is his gold complexion dimm'd;
+
+And every fair from fair sometime declines,
+
+By chance or nature’s changing course untrimm'd;
+
+But thy eternal summer shall not fade,
+
+Nor lose possession of that fair thou ow’st;
+
+Nor shall death brag thou wander’st in his shade,
+
+When in eternal lines to time thou grow’st:
+
+  So long as men can breathe or eyes can see,
+
+  So long lives this, and this gives life to thee.`,
+
+
+    "21": `She walks in beauty, like the night
+
+Of cloudless climes and starry skies;
+
+And all that’s best of dark and bright
+
+Meet in her aspect and her eyes;
+
+Thus mellowed to that tender light
+
+Which heaven to gaudy day denies.`,
+
    "a":`“This is how I would die
    into the love I have for you:
    As pieces of cloud
